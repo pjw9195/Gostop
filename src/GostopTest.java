9150896498b07b14a385322779b14be597dee134
jinupark <pjw9195@naver.com>
2017-05-28 03:59:26 +0900

    GoStop - 2(List)

--- src/GostopTest.java
+++ src/GostopTest.java
@@ -1,36 +1,95 @@
 import java.util.Random;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class GostopTest {
 
-	public static int Count(){
+	static List<Month> centerlist = new ArrayList<Month>();
+	static List<Month> playerlist = new ArrayList<Month>();
+	static List<Month> playerpanlist = new ArrayList<Month>();
+	protected static int b = 0;
+	protected static int Xrandom[] = new int[48];
+
+	public static int Count(){ //중복방지하면서 뽑는 랜덤변수
 		Random random = new Random();
 		boolean shift = true;
-		int Xrandom[] = new int[48];
-		int b = 0;
 		int a = 0;
 		while(shift){
+			shift = false;
 			a = random.nextInt(47)+1; //랜덤변수
 			for(int i = 0; i<48; i++){
 				if(Xrandom[i] == a){
 					shift = true;
 				}
-				if(i==47){
-					shift = false;
-				}
 			}
 		}
 		for(int i = 0; i<48;i++){ //중복방지하기위해 배열에 넣는다.
 			if(b == i){
-				Xrandom[b] = a;
-				b++;
+				Xrandom[i] = a;
 			}
 		}
+		b++;
+
 		return a;
 	}
+	public static void print2(){
+		
+		for(int i = 0; i<centerlist.size(); i++){
+		Month centerl1 = centerlist.get(i);
+		centerl1.print();
+		System.out.printf(" ");
+	}
+	System.out.printf("\n");
+	for(int i = 0; i<playerlist.size(); i++){
+		Month playerl1 = playerlist.get(i);
+		playerl1.print();
+		System.out.printf(" ");
+	}
+	System.out.printf("\n");
+	for(int i = 0; i<playerpanlist.size(); i++){
+		Month playerpanl1 = playerpanlist.get(i);
+		playerpanl1.print();
+		System.out.printf(" ");
+	}
+		
+	}
+	
 	public static void main(String[] args){
-		Month card = new Month();
-		int count = Count();
-		card.Card(count);
-		System.out.println(card.BasicScore);
+		Scanner sc = new Scanner(System.in);
+		////처음
+		for(int i= 0; i<7;i++){
+			Month card = new Month();
+			int count = Count();
+			card.Card(count);
+			centerlist.add(card);
+		}
+		System.out.printf(" \n");
+		for(int i= 0; i<10;i++){
+			Month card = new Month();
+			int count = Count();
+			card.Card(count);
+			playerlist.add(card);
+		}
+		print2();
+		
+		//패 옮기고 삭제하기
+		System.out.printf("\n");
+		System.out.printf("center몇번째패?");
+		int centerhand = sc.nextInt();
+		System.out.printf("player1몇번째패?");
+		int playerhand = sc.nextInt();
+		
+		Month centerl = centerlist.get(centerhand);
+		Month playerl = playerlist.get(playerhand);
+		playerpanlist.add(centerl);
+		playerpanlist.add(playerl);
+		centerlist.remove(centerhand);
+		playerlist.remove(centerhand);
+		
+		print2();
+		
+		
+		
 	}
 }
--- src/Month.java
+++ src/Month.java
@@ -1,17 +1,18 @@
 
 public class Month {
+	protected int a = 0;
+	protected int BasicScore = 0;
+	protected int Godori =0;
+	protected int Blue = 0;
+	protected int Red = 0;
+	protected int Cho = 0 ;
+	protected int Light = 0;
+	protected int Animal = 0;
+	protected int Dan = 0;
+	protected int BiLight = 0;
 	
-protected int BasicScore = 0;
-protected int Godori =0;
-protected int Blue = 0;
-protected int Red = 0;
-protected int Cho = 0 ;
-protected int Light = 0;
-protected int Animal = 0;
-protected int Dan = 0;
-protected int BiLight = 0;
 	public void Card(int a){
-	
+		this.a = a;
 		/////////////1월
 		if(a==1){
 			BasicScore++;
@@ -180,183 +181,169 @@ protected int BiLight = 0;
 		else if(a==48){
 			BiLight++; // 비광++
 			Light++;
-			
+
 		}
-	
+
 	}
-	public void print(int a){
-		
+	public void print(){
+
 		/////////////1월
 		if(a==1){
-			BasicScore++;
+			System.out.print("1월피");
 		}
 		else if(a==2){
-			BasicScore++;
+			System.out.print("1월피");
 		}
 		else if(a==3){
-			Red++;
-			Dan++;
+			System.out.print("1월홍단");
 		}
 		else if(a==4){
-			Light++;
+			System.out.print("1월광");
 		}
 		//////////////2월
 		else if(a==5){
-			BasicScore++;
+			System.out.print("2월피");
 		}
 		else if(a==6){
-			BasicScore++;
+			System.out.print("2월피");
 		}
 		else if(a==7){
-			Red++;
-			Dan++;
+			System.out.print("2월홍단");
 		}
 		else if(a==8){
-			Godori++;
-			Animal++;
+			System.out.print("2월고도리");
 		}
 		//////////////3월
 		else if(a==9){
-			BasicScore++;	
+			System.out.print("3월피");	
 		}
 		else if(a==10){
-			BasicScore++;
+			System.out.print("3월피");
 		}
 		else if(a==11){
-			Light++;
+			System.out.print("3월광");
 		}
 		else if(a==12){
-			Red++;
-			Dan++;
+			System.out.print("3월홍단");
 		}
 		//////////////4월
 		else if(a==13){
-			BasicScore++;
+			System.out.print("4월피");
 		}
 		else if(a==14){
-			BasicScore++;
+			System.out.print("4월피");
 		}
 		else if(a==15){
-			Godori++;
-			Animal++;
+			System.out.print("4월고도리");
 		}
 		else if(a==16){
-			Cho++;
-			Dan++;
+			System.out.print("4월초단");
 		}
 		//////////////5월
 		else if(a==17){
-			BasicScore++;
+			System.out.print("5월피");
 		}
 		else if(a==18){
-			BasicScore++;
+			System.out.print("5월피");
 		}
 		else if(a==19){
-			Animal++;
+			System.out.print("5월열끗");
 		}
 		else if(a==20){
-			Cho++;
-			Dan++;
+			System.out.print("5월초단");
 		}
 		//////////////6월
 		else if(a==21){
-			BasicScore++;
+			System.out.print("6월피");
 		}
 		else if(a==22){
-			BasicScore++;
+			System.out.print("6월피");
 		}
 		else if(a==23){
-			Animal++;
+			System.out.print("6월열끗");
 		}
 		else if(a==24){
-			Blue++;
-			Dan++;
+			System.out.print("6월청단");
 		}
 		//////////////7월
 		else if(a==25){
-			BasicScore++;
+			System.out.print("7월피");
 		}
 		else if(a==26){
-			BasicScore++;
+			System.out.print("7월피");
 		}
 		else if(a==27){
-			Cho++;
-			Dan++;
+			System.out.print("7월초단");
 		}
 		else if(a==28){
-			Animal++;
+			System.out.print("7월열끗");
 		}
 		//////////////8월
 		else if(a==29){
-			BasicScore++;
+			System.out.print("8월피");
 		}
 		else if(a==30){
-			BasicScore++;
+			System.out.print("8월피");
 		}
 		else if(a==31){
-			Godori++;
-			Animal++;
+			System.out.print("8월고도리");
 		}
 		else if(a==32){
-			Light++;
+			System.out.print("8월광");
 		}
 		//////////////9월
 		else if(a==33){
-			BasicScore++;
+			System.out.print("9월피");
 		}
 		else if(a==34){
-			BasicScore++;
+			System.out.print("9월피");
 		}
 		else if(a==35){
-			Blue++;
-			Dan++;
+			System.out.print("9월청단");
 		}
 		else if(a==36){
-			Animal++;
+			System.out.print("9월열끗");
 		}
 		//////////////10월
 		else if(a==37){
-			BasicScore++;
+			System.out.print("10월피");
 		}
 		else if(a==38){	
-			BasicScore++;
+			System.out.print("10월피");
 		}
 		else if(a==39){
-			Animal++;
+			System.out.print("10월열끗");
 		}
 		else if(a==40){
-			Blue++;
-			Dan++;
+			System.out.print("10월청단");
 		}
 		//////////////11월
 		else if(a==41){
-			BasicScore++;
+			System.out.print("11월피");
 		}
 		else if(a==42){
-			BasicScore++;
+			System.out.print("11월피");
 		}
 		else if(a==43){
-			BasicScore= BasicScore+2;
+			System.out.print("11월쌍피");
 		}
 		else if(a==44){
-			Light++;
+			System.out.print("11월광");
 		}
 		//////////////12월
 		else if(a==45){
-			BasicScore = BasicScore+2;
+			System.out.print("12월쌍피");
 		}
 		else if(a==46){
-			Animal++;	
+			System.out.print("12월열끗");
 		}
 		else if(a==47){
-			Dan++;
+			System.out.print("12월가짜초단");
 		}
 		else if(a==48){
-			BiLight++; // 비광++
-			Light++;
-			
+			System.out.print("12월비광");
 		}
-	
+
 	}
 
 }
